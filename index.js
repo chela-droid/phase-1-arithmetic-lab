e40e8b5b20727765a25129a3ab8f5dc769166d98
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-23 14:41:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// function multiply (num1,num2)
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
 
-// Write your code here
+const random = Math.floor(Math.random()*10)+1;
+console.log (random)
+//  if (random >0 );
+//   return 'true'
+
+
+// function mod (){
+//     // console.log (mod (34%10))
+
+let num3 = 34;
+let num4 = 10;
+let mod = num3 % num4;
+
+let  max=Math.max (3,5,7,2,1,12,18,20);
+console.log(max)
